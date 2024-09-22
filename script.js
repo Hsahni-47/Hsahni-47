b2957359011cdd6aa600d123a9a451a8eca6ebc2
Hsahni-47 <harshit.leeds@gmail.com>
2024-09-22 20:38:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for contacting me! I will get back to you shortly.');
-});
+
